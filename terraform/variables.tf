741745c2e760a62d5d55318c36bab5ddd4427798
Krishna Fattepurkar <krishnafattepurkar@gmail.com>
2025-11-03 21:07:00 +0530

    Refactor AWS variables for EKS configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,15 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
+variable "region" {
+  default = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-  default     = "insurance-predictor-cluster"
+  default = "ml-insurance-eks"
 }
 
-variable "subnet_ids" {
-  description = "List of subnet IDs for the EKS cluster"
-  type        = list(string)
+variable "node_instance_type" {
+  default = "t3.medium"
+}
+
+variable "desired_size" {
+  default = 1
 }
