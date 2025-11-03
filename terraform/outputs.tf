0d50f89e23ccc5d2e24a46fa8c7710c806cc3963
Krishna Fattepurkar <krishnafattepurkar@gmail.com>
2025-11-03 21:05:46 +0530

    added cluster name , endpoint and region

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,11 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS cluster"
-  value       = aws_eks_cluster.main.endpoint
-}
-
 output "cluster_name" {
-  description = "Name of the EKS cluster"
-  value       = aws_eks_cluster.main.name
+  value = aws_eks_cluster.main.name
 }
 
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = aws_eks_cluster.main.certificate_authority[0].data
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "region" {
+  value = var.region
 }
